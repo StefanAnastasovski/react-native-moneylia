3b88e3b01654cff3ef6c6ec0d63b9f30ce743e9f
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-09-10 00:37:39 +0200

    theme customized - dashboard

--- src/styles/moneyliaTheme.ts
+++ src/styles/moneyliaTheme.ts
@@ -6,9 +6,20 @@ export const moneyliaTheme = extendTheme({
     purpleBlue: {
       1: "#7476ED",
       2: "#3A3B7B",
+      3: "#312E43",
     },
     neutral: {
+      //light grey
       1: "#6F6D7B",
+      2: "#83828E",
+      3: "#A5A5AA",
+    },
+    errors: {
+      1: "#F83E5A", // red
+    },
+    background: {
+      1: "#E5E5E5", // white
+      2: "#F8F4FF", //violet
     },
   },
 });
