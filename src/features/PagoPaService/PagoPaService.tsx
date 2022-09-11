46a372f9765224bd1ac01d43a4cb55f500bb4a5b
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-09-12 01:42:44 +0200

    add PagoPaService

---
+++ src/features/PagoPaService/PagoPaService.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { View, useTheme, StatusBar } from "native-base";
+import { AppBar } from "../../components/AppBar/AppBar";
+import { PagoPaServiceCard } from "./components/PagoPaServiceCard/PagoPaServiceCard";
+
+const styles = {
+  container: {
+    flex: 1,
+    bg: "background.2",
+  },
+  rootContainer: {
+    flex: 1,
+  },
+  barStyle: "light-content",
+};
+
+export const PagoPaService = () => {
+  const {
+    colors,
+    colors: { lavender, background },
+  } = useTheme();
+
+  const extendedStyles = {
+    ...styles,
+    statusBar: {
+      flex: 0,
+      backgroundColor: lavender[1],
+    },
+    safeAreaViewBg: {
+      backgroundColor: background[2],
+    },
+  };
+  console.log(colors);
+  const { rootContainer, container, statusBar, safeAreaViewBg, barStyle } =
+    extendedStyles;
+
+  return (
+    <>
+      <StatusBar barStyle={barStyle} />
+      <SafeAreaView style={statusBar} />
+      <SafeAreaView style={[rootContainer, safeAreaViewBg]}>
+        <View {...container}>
+          <AppBar backgroundColor="lavender.1" />
+          <PagoPaServiceCard />
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
