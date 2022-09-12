33d7b5720b447af02806dfaf4fff61a86ce27da1
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-09-12 02:29:21 +0200

    add pagopa image

--- src/assets/allImages.ts
+++ src/assets/allImages.ts
@@ -1,3 +1,3 @@
 export const MoneyliaLogo = require("./brand/logo-moneylia.png");
 export const MoneyliaJoin = require("./images/moneylia-join-screen.png");
-
+export const PagoPaFrame = require("./images/pago-pa-frame.png");
