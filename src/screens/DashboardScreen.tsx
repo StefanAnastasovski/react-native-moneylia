9ef1f9abf7e9140c99eb92b93129686d7634b563
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-09-12 03:13:04 +0200

    add dashboard screen

---
+++ src/screens/DashboardScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Dashboard } from "../features/Dashboard/Dashboard";
+
+const DashboardScreen = () => {
+  return <Dashboard />;
+};
+
+export default DashboardScreen;
