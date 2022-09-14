1acca4273733fc1c1aa65d9108da6bc85a12a8b8
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-09-14 02:52:51 +0200

    fix Login layout

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -7,13 +7,15 @@ import JoinButton from "./components/JoinButton";
 import FindOutMore from "./components/FindOutMore";
 
 const Login = () => {
-  const { joinContainer } = styles;
+  const { container, joinContainer } = styles;
   return (
-    <View style={joinContainer}>
-      <JoinImage />
-      <JoinContent />
-      <JoinButton />
-      <FindOutMore />
+    <View {...container}>
+      <View style={joinContainer}>
+        <JoinImage />
+        <JoinContent />
+        <JoinButton />
+        <FindOutMore />
+      </View>
     </View>
   );
 };
@@ -21,6 +23,10 @@ const Login = () => {
 export default Login;
 
 const styles = {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
   joinContainer: {
     paddingLeft: 6,
     paddingRight: 6,
