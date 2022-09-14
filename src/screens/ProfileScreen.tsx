9533559941be8ff07cf1e06e5a3bad1d3d1c37a1
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-09-14 04:00:10 +0200

    add Log out in profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { View, Text, Center } from "native-base";
+import { View, Text, Center, Button } from "native-base";
 import { SafeAreaView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export const ProfileScreen = () => {
+  const navigation = useNavigation();
+
+  const { btnContainer, btn: btnStyle } = styles;
   return (
     <View flex={1} justifyContent="center">
       <SafeAreaView>
@@ -10,7 +14,32 @@ export const ProfileScreen = () => {
           <Text fontSize={50}>Profile</Text>
           <Text fontSize={32}>Coming Soon...</Text>
         </Center>
+
+        <View {...btnContainer}>
+          <Button {...btnStyle} onPress={() => navigation.navigate("Login")}>
+            Log Out
+          </Button>
+        </View>
       </SafeAreaView>
     </View>
   );
+};
+
+const styles = {
+  btnContainer: {
+    marginTop: 60,
+    marginBottom: 6,
+    alignItems: "center",
+  },
+  btn: {
+    height: 50,
+    width: 260,
+    backgroundColor: "purpleBlue.1",
+    _text: {
+      fontSize: 18,
+      fontWeight: 600,
+      lineHeight: 18,
+      color: "white",
+    },
+  },
 };
