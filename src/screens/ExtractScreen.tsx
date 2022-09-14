0296460e45ee372496f7c4fbf5a466bdd0398f82
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-09-14 02:52:24 +0200

    add empty screens

---
+++ src/screens/ExtractScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "native-base";
+
+export const ExtractScreen = () => {
+  return (
+    <View>
+      <Text>Coming Soon...</Text>
+    </View>
+  );
+};
