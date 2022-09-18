5f8ca7205e5f2d3d9d73dc0d682fcad041f37714
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-09-18 23:36:02 +0200

    fix navigation bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 // imports: libraries + components
 import React, { useEffect } from "react";
 import { NativeBaseProvider } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
 
 import SplashScreen from "react-native-splash-screen";
 // imports
 import { moneyliaTheme } from "./styles/moneyliaTheme";
-import { featureFlags } from "./data/featureFlags";
-import { NavigationContainer } from "@react-navigation/native";
 import { MainNavigation } from "./navigation/MainNavigation";
-// import { BottomTabNavigation } from "./navigation/BottomTabNavigation";
 
 export const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
-
-  // using flags to enable/disable screens => remove flags when navigation will be added.
-  const {
-    isJoinFreeEnabled: joinFreeFlag,
-    isDashboardEnabled: dashboardFlag,
-    isPagoPaServiceEnabled: pagoPaServiceFlag,
-    isPaymentDetailsEnabled: paymentDetailsFlag,
-  } = featureFlags;
 
   return (
     <>
--- src/data/featureFlags.ts
+++
@@ -1,6 +0,0 @@
-export const featureFlags = {
-  isJoinFreeEnabled: false,
-  isDashboardEnabled: false,
-  isPagoPaServiceEnabled: false,
-  isPaymentDetailsEnabled: true,
-};
